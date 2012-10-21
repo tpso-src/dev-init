051de43d51f95e5bdec8fc3aeefc6b3c0958bfe0
root <root@localhost.localdomain>
2012-10-21 04:22:39 +0200

    Addet ant link

--- bin/env.sh
+++ bin/env.sh
@@ -1,3 +1,4 @@
 export TOPDIR=/root/dev
 
 export PATH=${TOPDIR}/dev-init/apps/ant/bin:${TOPDIR}/dev-init/apps/java/bin:$PATH
+export CLASSPATH=/tmp/xerces-2_11_0/xml-apis.jar:/tmp/xerces-2_11_0/xercesImpl.jar
